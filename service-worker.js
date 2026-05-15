1de5a50d2329dc6e92300cd7b5201189dc267f4e
saraa-siva <studybutterfly100@gmail.com>
2026-05-15 15:59:01 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installing...");
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  console.log("SW activated");
 });
 
 self.addEventListener("fetch", (event) => {
